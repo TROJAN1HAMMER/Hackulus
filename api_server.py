61e4a5c8102476ea43441c8e5088843bea78150e
TROJAN1HAMMER <balaachuanu@gmail.com>
2025-09-21 15:57:18 +0530

    cors added

--- api_server.py
+++ api_server.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
 import joblib
 import pandas as pd
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 # --- 1. Load the trained model ---
 # This line loads your trained AI model into memory when the server starts.
